3056e84d4cec65afd702b15043f0ecdf3f9642d9
Ayo Ayco <ayo@ayco.io>
2025-03-22 10:11:48 +0100

    test: set up vitest & add tests

---
+++ src/WebComponent.test.mjs
@@ -0,0 +1,25 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { WebComponent } from './WebComponent.js'
+
+let componentUnderTest
+
+describe('WebComponent', () => {
+  // Browsers throw an error when you instantiate a custom element class not in the registry
+  window.customElements.define('component-test', WebComponent)
+
+  beforeEach(() => {
+    componentUnderTest = new WebComponent()
+  })
+
+  it('is instantiated', () => {
+    expect(componentUnderTest).toBeDefined()
+  })
+
+  it('has a readonly template prop', () => {
+    expect(componentUnderTest.template).toBe('')
+    const assignToReadonly = () => {
+      componentUnderTest.template = 'should error'
+    }
+    expect(() => assignToReadonly()).toThrowError()
+  })
+})
---
+++ vitest.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    environment: 'happy-dom',
+    coverage: {
+      enabled: true,
+      provider: 'v8',
+      reporter: ['html', 'text'],
+      include: ['src'],
+    },
+  },
+})
