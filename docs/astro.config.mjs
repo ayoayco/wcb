3358e7c80539dbbf676d917c7cd19cbc7541b949
Ayo Ayco <ayo@ayco.io>
2025-03-26 19:50:07 +0100

    feat(docs): set up first guide page

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,28 +1,32 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from 'astro/config'
+import starlight from '@astrojs/starlight'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
-});
+  redirects: {
+    '/': '/guides',
+  },
+  integrations: [
+    starlight({
+      title: 'WCB',
+      social: {
+        npm: 'https://www.npmjs.com/package/web-component-base',
+        sourcehut: 'https://sr.ht/~ayoayco/wcb/',
+      },
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Getting Started', slug: 'guides' },
+          ],
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+      ],
+    }),
+  ],
+})
