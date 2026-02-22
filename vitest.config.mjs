b1320a05f3195abeb6fc2f8a11289fcd9e2565be
Ayo <ayo@ayco.io>
2026-02-22 15:58:07 +0100

    chore: exclude files on vitest; remove sr.ht build.yml

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -3,6 +3,11 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     environment: 'happy-dom',
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      './temp/**',
+    ],
     coverage: {
       enabled: true,
       provider: 'v8',
